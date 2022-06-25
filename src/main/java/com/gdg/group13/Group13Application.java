6556813f39daaf341212b2cc7b40751b007f62f2
ritty27 <jspark9812@gmail.com>
2022-06-26 03:29:37 +0900

    fix: base entity auditing

--- src/main/java/com/gdg/group13/Group13Application.java
+++ src/main/java/com/gdg/group13/Group13Application.java
@@ -2,8 +2,10 @@ package com.gdg.group13;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Group13Application {
 
 	public static void main(String[] args) {
